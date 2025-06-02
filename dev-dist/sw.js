54320e09d8b8a7359deac9c57e16ec43b7f074ae
manuelmendezac <homodeus.cith@gmail.com>
2025-06-02 21:55:30 +0300

    feat: agregar plantilla Señales Pro personalizable y corregir editor

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tec9o3tjp38"
+    "revision": "0.kr0dmrv7gd"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
