ea99e39adbe7d4f7a41d459f8de03a10462ba24e
manuelmendezac <homodeus.cith@gmail.com>
2025-06-02 19:41:27 +0300

    fix: elimina página huérfana SegundoCerebro.tsx que causaba error de build

--- src/pages/SegundoCerebro.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import SegundoCerebroPage from '../components/pages/SegundoCerebroPage';
-
-export default function SegundoCerebro() {
-  return <SegundoCerebroPage />;
-} 
