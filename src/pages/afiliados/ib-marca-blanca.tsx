c99c87140e7097f5459ca23ec0d7b1976f84f859
manuelmendezac <homodeus.cith@gmail.com>
2025-06-09 22:28:21 +0400

    fix: Barra lateral siempre visible en Comisiones Marca Blanca (usa AfiliadosLayout)

--- src/pages/afiliados/ib-marca-blanca.tsx
+++ src/pages/afiliados/ib-marca-blanca.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import AfiliadosLayout from '../../components/afiliados/AfiliadosLayout';
 import InformeIBMarcaBlanca from '../../components/afiliados/InformeIBMarcaBlanca';
 
-const IBMarcaBlancaPage = () => <InformeIBMarcaBlanca />;
+const IBMarcaBlancaPage = () => (
+  <AfiliadosLayout>
+    <InformeIBMarcaBlanca />
+  </AfiliadosLayout>
+);
 
 export default IBMarcaBlancaPage; 
