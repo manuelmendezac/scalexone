f01a99ac99f32200630f61aaabc158aad4bb487c
manuelmendezac <homodeus.cith@gmail.com>
2025-06-18 03:05:57 +0400

    Revert "test: forzar status 200 y Content-Length en OG tags para intentar compatibilidad con Facebook/WhatsApp"
    This reverts commit 33fd2d7161d62f4818ad053e475fe13f9c592f00.

--- pages/api/share/[postId].ts
+++ pages/api/share/[postId].ts
@@ -7,7 +7,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const image = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb';
   const realUrl = `https://scalexone.app/comunidad?post=${postId}`;
 
-  const html = `
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.status(200).send(`
     <!DOCTYPE html>
     <html lang="es">
       <head>
@@ -26,9 +27,5 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         <p><a href="${realUrl}">Ver post en ScalexOne</a></p>
       </body>
     </html>
-  `;
-
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Content-Length', Buffer.byteLength(html, 'utf-8').toString());
-  res.status(200).send(html);
+  `);
 } 
