0be354631f80cce899d6b15c9094f745177b88f2
manuelmendezac <homodeus.cith@gmail.com>
2025-06-20 23:24:28 +0400

    chore: Force deploy

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,14 @@ import { router } from './router'
 import './index.css'
 import { ThemeProvider } from './context/themeContext'
 import './i18n'
+import App from './App.tsx'
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n'; // Asegúrate de que la ruta es correcta
+import { register } from 'swiper/element/bundle';
+
+// Force deploy: 2024-07-29 12:00:00
+
+register();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
