514101fa6082fe87b563268a511fd633757bce98
manuelmendezac <homodeus.cith@gmail.com>
2025-06-26 10:27:31 +0400

    Ajustar diseño para ancho completo en el panel de administración

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -6,9 +6,9 @@ export default function AdminPanel() {
   const [activeTab, setActiveTab] = useState('miembros');
 
   return (
-    <div className="flex">
+    <div className="flex w-full">
       <AdminSidebar selected={activeTab} onSelect={setActiveTab} />
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         {activeTab === 'miembros' && <MiembrosAdminPanel />}
         {/* Render other components based on activeTab */}
       </div>
